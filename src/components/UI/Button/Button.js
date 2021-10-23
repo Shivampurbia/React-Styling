6a4982d4fb340c9a21a69c4e8d6a940c2a082395
Shivampurbia <shivam3265prb@gmail.com>
2021-10-23 16:31:49 +0530

    css module dynamic

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,10 @@
-import React from 'react';
-
-import './Button.css';
+import react from 'react';
+import styled from 'styled-components';
+import styles from  './Button.module.css';
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
